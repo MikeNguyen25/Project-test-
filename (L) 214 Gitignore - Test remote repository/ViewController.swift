63eee335a89dc65e78d5f68db5f5ab4ef2d60869
Manh Phuc <apple@apples-mbp.dsldevice.lan>
2019-07-25 19:28:29 +0700

    Initial commit 1

---
+++ (L) 214 Gitignore - Test remote repository/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  (L) 214 Gitignore - Test remote repository
+//
+//  Created by Manh Phuc on 7/25/19.
+//  Copyright © 2019 Mike Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
